9e80f4143fe41ad0d85886f008354dc1b4018698
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2026-06-25 12:26:21 +1000

    feat: finished tutorial 5 (going by repo)
    up to 6 now

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,5 +17,15 @@ def index():
         }
         
     ]
-    return render_template('index.html', title= 'Moose Blog', user=user)
+    return render_template('index.html', title= 'Moose Blog', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/no_title')
+def no_title():
+    user = {'username': 'Moose'}
+    return render_template('index.html', user=user)
 
